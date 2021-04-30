04f905689060ef78809377542be7c7aea110a154
Josh Breuer <>
2021-04-29 23:55:13 -0400

    add child-src blob directive for mapbox

--- app.js
+++ app.js
@@ -110,6 +110,7 @@ app.use(
 			scriptSrc  : [ "'unsafe-inline'", "'self'", ...scriptSrcUrls ],
 			styleSrc   : [ "'self'", "'unsafe-inline'", ...styleSrcUrls ],
 			workerSrc  : [ "'self'", 'blob:' ],
+			childSrc   : [ 'blob:' ],
 			objectSrc  : [],
 			imgSrc     : [
 				"'self'",
