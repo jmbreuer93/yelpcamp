d1365152e2bd3fc58cbda8f4ca7a40a14a530f5d
Josh Breuer <>
2021-04-29 23:45:36 -0400

    update allowed script URLs

--- app.js
+++ app.js
@@ -84,23 +84,23 @@ app.use(flash());
 app.use(helmet());
 
 const scriptSrcUrls = [
-	'https://stackpath.bootstrapcdn.com/',
-	'https://api.tiles.mapbox.com/',
-	'https://api.mapbox.com/',
-	'https://kit.fontawesome.com/',
-	'https://cdnjs.cloudflare.com/',
+	'https://stackpath.bootstrapcdn.com',
+	'https://api.tiles.mapbox.com',
+	'https://api.mapbox.com',
+	'https://kit.fontawesome.com',
+	'https://cdnjs.cloudflare.com',
 	'https://cdn.jsdelivr.net'
 ];
 const styleSrcUrls = [
-	'https://kit-free.fontawesome.com/',
-	'https://stackpath.bootstrapcdn.com/',
-	'https://api.mapbox.com/',
-	'https://api.tiles.mapbox.com/',
-	'https://fonts.googleapis.com/',
-	'https://use.fontawesome.com/',
+	'https://kit-free.fontawesome.com',
+	'https://stackpath.bootstrapcdn.com',
+	'https://api.mapbox.com',
+	'https://api.tiles.mapbox.com',
+	'https://fonts.googleapis.com',
+	'https://use.fontawesome.com',
 	'https://cdn.jsdelivr.net'
 ];
-const connectSrcUrls = [ 'https://api.mapbox.com/', 'https://*.tiles.mapbox.com/', 'https://events.mapbox.com/' ];
+const connectSrcUrls = [ 'https://api.mapbox.com', 'https://*.tiles.mapbox.com', 'https://events.mapbox.com' ];
 const fontSrcUrls = [];
 app.use(
 	helmet.contentSecurityPolicy({
